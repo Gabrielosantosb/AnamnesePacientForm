0ea0aec784b13e3b72186140bd9517160d97061f
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-27 15:51:39 -0300

    Adicionando service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacientComponent } from './modules/pacient/pacient.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
@@ -19,6 +19,7 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    FormsModule,
 
   ],
   providers: [],
--- src/app/modules/pacient/pacient.component.ts
+++ src/app/modules/pacient/pacient.component.ts
@@ -11,14 +11,14 @@ import {PacientRequest} from "../../model/pacient/PacientRequest";
 export class PacientComponent {
   isLoading = false;
   public pacientForm = this.formBuilder.group({
-    username: ['', Validators.required],
-    email: ['', Validators.required],
-    address: ['', Validators.required],
-    uf: ['', Validators.required],
-    phone: ['', Validators.required],
-    birth: ['', [Validators.required, this.dateValidator]],
-    gender: ['', Validators.required],
-    profession: ['', Validators.required],
+    username: ['Gabriel', Validators.required],
+    email: ['gabriel@email.com', Validators.required],
+    address: ['qnj 32 22', Validators.required],
+    uf: ['DF', Validators.required],
+    phone: ['61982429000', Validators.required],
+    birth: ['2024-02-05', [Validators.required, this.dateValidator]],
+    gender: ['MASCULINO', Validators.required],
+    profession: ['Vagabundo', Validators.required],
   });
   constructor(private formBuilder: FormBuilder,
               private pacientService: PacientService
@@ -35,24 +35,24 @@ export class PacientComponent {
     return null;
   }
 
-  // handlePacientForm(): void {
-  //   if (this.pacientForm?.value && this.pacientForm.valid) {
-  //     this.isLoading = true
-  //     const requestPacient = this.pacientForm.value as PacientRequest
-  //     this.pacientService.createPacient(requestPacient)
-  //       .subscribe({
-  //         next: (response) => {
-  //           if(response){
-  //             this.isLoading = false;
-  //             console.log("Foi", response)
-  //           }
-  //         },
-  //         error :(err) =>{
-  //           console.log(err)
-  //           this.pacientForm.reset()
-  //         }
-  //       })
-  //   }
-  // }
+  handlePacientForm(): void {
+    if (this.pacientForm?.value && this.pacientForm.valid) {
+      this.isLoading = true
+      const requestPacient = this.pacientForm.value as PacientRequest
+      this.pacientService.createPacient(requestPacient)
+        .subscribe({
+          next: (response) => {
+            if(response){
+              this.isLoading = false;
+              console.log("Foi", response)
+            }
+          },
+          error :(err) =>{
+            console.log(err)
+            this.pacientForm.reset()
+          }
+        })
+    }
+  }
 
 }

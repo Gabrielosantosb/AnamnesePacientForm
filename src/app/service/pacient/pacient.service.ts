fed5bc7a871d4485b1bee2f87106934077cc255e
Gabriel Santos <gabrielosantosb@outlook.com>
2024-03-27 16:04:08 -0300

    Criando usario funcionando

--- src/app/service/pacient/pacient.service.ts
+++ src/app/service/pacient/pacient.service.ts
@@ -13,7 +13,8 @@ import {PacientsResponse} from "../../model/pacient/PacientResponse";
 export class PacientService {
   private API_URL = environments.API_URL
   private readonly USER_AUTH = environments.COOKIES_VALUE.user_auth
-  private token = this.cookie.get(this.USER_AUTH)
+  // private token = this.cookie.get(this.USER_AUTH)
+  private token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiR2FicmllbCBCYXJib3NhIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiIxIiwiZXhwIjoxNzM2NzkwNTU3LCJpc3MiOiJHYWJyaWVsLm5ldCIsImF1ZCI6Imh0dHA6Oi8vd3d3LmdhYnJpZWwubmV0In0.cVdQ-Rw3NWo9ZuJ8tDbu11yXkorhRZDer3YfC8MEe0w'
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
